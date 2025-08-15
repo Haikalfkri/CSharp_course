a462d0e12a77d36e923b16389bcb8798692d58cf
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 06:52:13 +0700

    First Program

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.Write("Hello!"); // This line prints "Hello" without a newline
+            Console.WriteLine("Hello!"); // This line prints "Hello!" with a newline
+
+            // This is a comment
+            /*
+             this is a multi-line comment
+             */
+
+            Console.ReadKey(); // Waits for a key press before closing the console window
+        }
+    }
+}
