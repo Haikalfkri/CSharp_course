7b77b4f53aa3b1b5f9bdb7f99e3ec5aa13997bde
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 14:04:38 +0700

    user input

---
+++ userInput/userInput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace userInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your first name: ");
+            String firstName = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            String lastName = Console.ReadLine();
+
+            Console.Write("Enyer your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your name is " + firstName + " " + lastName + " and you are " + 
+                                age + " years old"); 
+
+        }
+    }
+}
