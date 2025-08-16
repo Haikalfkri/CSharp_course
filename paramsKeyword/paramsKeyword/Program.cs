f79ab22fc43ca9d699db911e1a729c3b8f60baf2
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 18:44:33 +0700

    params keyword

---
+++ paramsKeyword/paramsKeyword/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // params keyword = a method parameter that takes variable number of arguments
+
+            double total = checkOut(19.99, 5.49, 3.99, 12.50, 7.25);
+            Console.WriteLine(total);
+        }
+
+        static double checkOut(params double[] prices)
+        {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price;
+            }
+
+            return total;
+        }
+    }
+}
