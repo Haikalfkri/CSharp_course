1f0fe2d4da6ddfd637f879385efc85c97e20c061
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 14:41:16 +0700

    string methods

---
+++ stringMethods/stringMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace stringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "John Doe";
+            String phoneNumber = "123-456-7890";
+
+            // fullName = fullName.ToUpper();
+            fullName = fullName.ToLower();
+            Console.WriteLine(fullName);
+
+            phoneNumber = phoneNumber.Replace("-", "");
+            Console.WriteLine(phoneNumber);
+
+            String username = fullName.Insert(0, "mr.");
+            Console.WriteLine(username);
+
+            Console.WriteLine(fullName.Length); // Outputs the length of the string
+
+            String firstName = fullName.Substring(0, 4); // Extracts "John"
+            String lastName = fullName.Substring(5, 3); // Extracts "Doe"
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+        }
+    }
+}
