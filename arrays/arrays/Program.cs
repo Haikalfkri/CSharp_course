e2928b809ae47e90f9c8cb34b3362e0cff5917d8
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 17:31:53 +0700

    arrays

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array = a variable that can store multiple values
+
+            String[] cars = new string[3]; // fixed size
+
+            // String[] cars = { "BMW", "Mustang", "Tesla" };
+
+            cars[0] = "Mustang";
+            cars[1] = "BMW";
+            cars[2] = "Tesla";
+
+            //Console.WriteLine(cars[0]);
+            //Console.WriteLine(cars[1]);
+            //Console.WriteLine(cars[2]);
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+        }
+    }
+}
