ebc3ee7a7f3d10a68770a6a7403dd2a4c3c494cd
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 16:36:20 +0700

    nested loop

---
+++ nestedLoops/nestedLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace nestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // nested loops = loop inside of other loop
+
+            Console.Write("How many rows? ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many columns? ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What symbol? ");
+            string symbol = Console.ReadLine();
+
+
+            for ( int i = 1; i <= rows; i++ )
+            {
+                for ( int j = 1; j <= cols; j++ )
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
