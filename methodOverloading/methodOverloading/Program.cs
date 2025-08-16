1ce1ef0bfc9ebcf792caa85cdeb28cc4870c2520
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 18:38:24 +0700

    method overloading

---
+++ methodOverloading/methodOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method overloading = methods with the same name but different parameters
+
+            double total;
+            total = multiply(2, 3); // 2 parameters
+            total = multiply(2, 3, 4); // 3 parameters
+            Console.WriteLine(total);
+
+        }
+
+        static double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+    }
+}
