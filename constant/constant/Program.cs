91ad2597dc8658db97d29ed60c3be6ed5b0309de
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 13:50:37 +0700

    constants

---
+++ constant/constant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constant = immutable variable
+
+            const double pi = 3.14159;
+
+            Console.WriteLine("The value of pi is: " + pi);
+        }
+    }
+}
