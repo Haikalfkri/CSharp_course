5c6e98d0affde83257af72cc45dfb4833bc1ef78
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 16:28:24 +0700

    for loop

---
+++ forLoops/forLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace forLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop = repeats a section of code a limited number of times
+            /*
+            for ( int i = 1; i <= 10; i++ )
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+            for ( int i = 10; i > 0; i-- )
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
