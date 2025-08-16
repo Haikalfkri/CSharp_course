6f0f66a061cd29bb1b6cfd96ea2fc4b7b5875f05
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 17:35:59 +0700

    foreach loop

---
+++ foreachLoop/foreachLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace foreachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop = a simpler way to iterate through an array, but it's less flexible
+
+            String[] cars = { "BMW", "Tesla", "Ford", "Toyota" };
+
+            // Using foreach to iterate through the array
+            foreach( String car in cars )
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
