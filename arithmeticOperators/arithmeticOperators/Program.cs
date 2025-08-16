4a126b578039fea587587fa2ed4315c255fd4e9e
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 14:08:56 +0700

    arithmetic operators

---
+++ arithmeticOperators/arithmeticOperators/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace arithmeticOprators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            // addition
+            // num = num + 2;
+            // num += 2;
+            // num++; // increment by 1
+
+            // subtraction
+            // num = num - 2;
+            // num -= 2;
+            // num--; // decrement by 1
+
+            // multiplication
+            // num = num * 2;
+            // num *= 2;
+
+            // division
+            // num = num / 2;
+            // num /= 2;
+
+            // modulus
+            // num = num % 2;
+            // num %= 2;
+        }
+    }
+}
