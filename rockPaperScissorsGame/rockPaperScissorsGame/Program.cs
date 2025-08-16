913c52ebbf224c1944dc9eda031769295ac4c63d
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 17:21:06 +0700

    rock, paper, scissors game

---
+++ rockPaperScissorsGame/rockPaperScissorsGame/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            String response;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+
+                while (player != "rock" && player != "paper" && player != "scissors")
+                {
+                    Console.WriteLine("Enter rock, paper, or scissors:");
+                    player = Console.ReadLine().ToLower();
+                }
+
+                int randomNum = random.Next(1, 4);
+                switch (randomNum)
+                {
+                    case 1:
+                        computer = "rock";
+                        break;
+                    case 2:
+                        computer = "paper";
+                        break;
+                    case 3:
+                        computer = "scissors";
+                        break;
+                }
+
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+
+
+                switch (player)
+                {
+                    case "rock":
+                        if (computer == "rock")
+                        {
+                            Console.WriteLine("It's a tie!");
+                        }
+                        else if (computer == "paper")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        break;
+                    case "paper":
+                        if (computer == "rock")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else if (computer == "paper")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        break;
+                    case "scissors":
+                        if (computer == "rock")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else if (computer == "paper")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        break;
+                }
+
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                response = Console.ReadLine().ToLower();
+
+                if (response == "yes")
+                {
+                    playAgain = true;
+                }
+                else if (response == "no")
+                {
+                    playAgain = false;
+                } else
+                {
+                    Console.WriteLine("Invalid response, try again!");
+                }
+            }
+            Console.WriteLine("Thanks for playing!!");
+        }
+    }
+}
