a6b305bff1a6ce7341f2cba1897ce204e1cbd553
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-16 15:30:58 +0700

    while loops

---
+++ whileLoops/whileLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace whileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while loop = repeats some code while a condition is true
+
+            string username = "";
+
+            while (username == "")
+            {
+                Console.Write("Enter your username: ");
+                username = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your username is {username}");
+        }
+    }
+}
