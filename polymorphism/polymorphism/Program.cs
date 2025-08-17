75a73915204db644fca3425503103325e9c16e9c
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 11:07:37 +0700

    polymorphism

---
+++ polymorphism/polymorphism/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // polymorphism = Greek word that means "many forms"
+            //                object can be identified by more than one type
+
+            Bicycle bicycle = new Bicycle();
+            Car car = new Car();
+            Truck truck = new Truck();
+
+            Vehicle[] vehicles = { bicycle, car, truck };
+
+            foreach (Vehicle vehicle in vehicles)
+            {
+                vehicle.Go();
+            }
+        }
+    }
+
+    class Vehicle
+    {
+        public virtual void Go()
+        {
+            Console.WriteLine("Vehicles is moving");
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+    class Car : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+    class Truck : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Truck is moving");
+        }
+    }
+}
