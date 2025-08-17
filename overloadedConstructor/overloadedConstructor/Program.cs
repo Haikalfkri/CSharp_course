e5b3bceabe5e4079e475cf3fefd5c5abd3a41fe7
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 09:52:12 +0700

    overloaded constructor

---
+++ overloadedConstructor/overloadedConstructor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace overloadedConstractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // overloaded constructor = technique to create multiple constructors,
+            // each with different parameters, to create objects in different ways
+
+            Pizza pizza1 = new Pizza("Thin Crust", "Tomato Sauce", "Pepperoni");
+        }
+    }
+
+    class Pizza
+    {
+        String bread;
+        String sauce;
+        String topping;
+        String cheese;
+
+        public Pizza (string bread, string sauce)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+        } 
+
+        public Pizza(string bread, string sauce, string topping)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.cheese = cheese;
+        }
+
+        public Pizza(string bread, string sauce, string cheese, string topping)
+        {
+            this.bread = bread;
+            this.sauce = sauce;
+            this.topping = topping;
+            this.cheese = cheese;
+        }
+    }
+}
