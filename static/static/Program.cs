84137a0c04dc9274ca9a3b9179d82d4ae28c88b3
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 09:43:44 +0700

    static

---
+++ static/static/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // static = modifier to declare a static member, which belongs to the class
+            //          itself rather than to instances of the class
+
+            Car car1 = new Car("Toyota");
+            Car car2 = new Car("Honda");
+
+            Console.WriteLine(Car.numberOfCars);
+        }
+    }
+
+    class Car
+    {
+        String model;
+        public static int numberOfCars;
+
+        public Car(String model)
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+    }
+}
