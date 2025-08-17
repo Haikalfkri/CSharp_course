32055713d6dc918c098a8bbf2c684add566b530e
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 08:41:42 +0700

    conditional operator

---
+++ conditionalOperator/conditionalOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // conditional operator = used in conditional assignment if a condition is true or false
+            // example (condition) ? trueValue : falseValue;
+
+            double temperature = 20;
+            String message;
+
+            // Using if statement
+            //if (temperature >= 15)
+            //{
+            //    message = "It's warm outside.";
+            //} 
+            //else
+            //{
+            //    message = "It's cold outside.";
+            //}
+
+            // Using the conditional operator to determine if it's hot or cold
+            message = (temperature >= 15) ? "It's warm outside." : "It's cold outside.";
+
+            Console.WriteLine(message);
+        }
+    }
+}
