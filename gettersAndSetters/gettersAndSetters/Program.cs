5c8237be38d83fcc7e05fcc452a32eaf1e2592a2
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 13:10:14 +0700

    getters and setters

---
+++ gettersAndSetters/gettersAndSetters/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // getters and setters = add security to fields by encapsulating them
+            //                       they are accessors found within properties
+
+            Car car = new Car(100);
+
+            car.Speed = 700;
+
+            Console.WriteLine(car.Speed);
+        }
+    }
+
+    class Car
+    {
+        private int speed;
+
+        public Car(int speed)
+        {
+            Speed = speed; // using the property to set the value
+        }
+
+        public int Speed
+        {
+            get { return speed; } // read
+            set                   // write
+            {
+                if (value > 500)
+                {
+                    speed = 500;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+    }
+}
