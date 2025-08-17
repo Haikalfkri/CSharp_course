025e75712cb61fd7289b2e07840640f0fca55d84
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 08:46:20 +0700

    string interpolation

---
+++ stringInterpolation/stringInterpolation/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace myProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string interpolation = allows us to embed expressions within string literals
+            // syntax: $"string {expression} string"
+
+            String name = "Alice";
+            int age = 25;
+
+            Console.WriteLine($"Your name is {name} and you are {age} years old"); // string interpolation
+        }
+    }
+}
