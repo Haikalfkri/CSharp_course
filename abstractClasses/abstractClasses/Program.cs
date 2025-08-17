3638644cec8508d57a4eab55c5eb5e6c1d8e934b
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 10:07:01 +0700

    abstract class

---
+++ abstractClasses/abstractClasses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace abstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inheritance = 1 or more child classes receiving fields, methods, etc. from a parent class
+
+            Car car1 = new Car();
+            Bicycle bicycle1 = new Bicycle();
+            //Vehicle vehicle1 = new Vehicle();
+        }
+    }
+
+    abstract class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!!");
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 20;
+    }
+    class Motorcycle : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 100;
+    }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+        int maxSpeed = 200;
+    }
+}
