f10871d22e14cce1301d5dc696dcd745143360fc
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 12:59:59 +0700

    list of objects

---
+++ listOfObjects/listOfObjects/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace listOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            //Player player1 = new Player("Player1");
+            //Player player2 = new Player("Player2");
+            //Player player3 = new Player("Player3");
+
+            players.Add(new Player("player1"));
+            players.Add(new Player("player2"));
+            players.Add(new Player("player3"));
+
+            foreach (Player player in players)
+            {
+                //Console.WriteLine(player.username);
+                Console.WriteLine(player);
+            }
+
+        }
+    }
+
+    class Player
+    {
+        public string username;
+
+        public Player(string username)
+        {
+            this.username = username;
+        }
+
+        public override string ToString()
+        {
+            return username;
+        }
+    }
+}
