50fa605960d68e6f0486af2649ec0123bb445008
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 09:15:32 +0700

    classes

---
+++ classes/classes/Messages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    static class Messages
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        public static void Hello()
+        {
+            Console.WriteLine("Hello, nice to meet you!!");
+        }
+
+        public static void GoodBye()
+        {
+            Console.WriteLine("Good Bye!!");
+        }
+
+        public static void HowAreYou()
+        {
+            Console.WriteLine("How are you?");
+        }
+    }
+}
---
+++ classes/classes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class = A bundle of related code.
+            // can be used as a blueprint to create objects (OOP).
+
+            Messages.Welcome();
+            Messages.Hello();
+        }
+    }
+
+    // class = A bundle of related code.
+    /*
+    class Messages
+    {
+        void Welcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        void Hello()
+        {
+            Console.WriteLine("Hello, nice to meet you!!");
+        }
+
+        void GoodBye()
+        {
+            Console.WriteLine("Good Bye!!");
+        }
+
+        void HowAreYou()
+        {
+            Console.WriteLine("How are you?");
+        }
+    }
+    */
+}
