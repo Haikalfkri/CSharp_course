02a779cfb490ad2bac833db3ff414c144bda7b89
Haikal Fikri <haikaljr1245@gmail.com>
2025-08-17 13:19:05 +0700

    auto-implemented property

---
+++ autoImplementedProperties/autoImplementedProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace autoImplementedProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Auto-Implemented properties = shortcut when no additional logic is required in property
+
+            Car car = new Car("Lambo");
+
+            Console.WriteLine(car.Model);
+        }
+    }
+
+    class Car
+    {
+        //string model;
+
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+
+        public string Model { get; set; } // Auto-Implemented property
+
+        //public string Model
+        //{
+        //    get { return model; }
+        //    set
+        //    {
+        //        model = value;
+        //    }
+        //}
+    }
+}
